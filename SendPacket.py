770c426cc4ec901bf5e14f7eb795ad7da42296a5
nameisntimportant <32089454+nameisntimportant@users.noreply.github.com>
2019-05-21 09:20:51 +0800

    v1.0
    after mask
    before function name & file

---
+++ SendPacket.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 15 23:02:48 2019
+
+@author: DELL
+"""
+
+# -*- coding:utf-8 -*-
+# 通过给节点发送一个包含指令（send）的数据包来操控节点发送数据
+
+import socket
+from DataStructure import *
+
+
+src = input("Input the source of this packet(eg: 'A', 'B'..., 'E'): ")
+dest =  input("Input the destination of this packet(eg: 'A', 'B'..., 'E'): ")
+
+srcAddr = name_To_address(src)
+
+commandSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  #  采用UDP
+# commandSocket.bind(('127.0.0.1', 29999))
+
+payload = dest
+sendpkt = Packet(Address('127.0.0.1', 24, 29999), srcAddr, payload, 2)
+commandSocket.sendto(sendpkt.tojson().encode(), (srcAddr.ip, srcAddr.port))
+# © 2019 GitHub, Inc.
